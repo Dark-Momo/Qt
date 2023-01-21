9c5a0545effc675b70ef8cb4a4d50acc269c9373
Jian Lao <jian.lao@simplisafe.com>
2023-01-20 21:55:42 -0500

    Ep13...

--- Qt6_VoidRealms_Ep13_QMap/main.cpp
+++ Qt6_VoidRealms_Ep13_QMap/main.cpp
@@ -6,7 +6,37 @@
 #include "person.h"
 
 // Use a QString 'nickName' to associate with a person object.
-typedef QMap<QString, QSharedPointer<person>> nickName;
+typedef QMap<QString, QSharedPointer<person>> NickName;
+
+void setPersonInstance(NickName &family)
+{
+    // NickName family;
+    for (int i = 0; i < 3; i++)
+    {
+        QSharedPointer<person> ptr(new person());
+        ptr->setName("UnkownName" + QString::number(i));
+        ptr->setAge(i);
+        family.insert("UnkownPerson" + QString::number(i), ptr);
+    }
+}
+
+void showPersonInstance(NickName &family)
+{
+    /*
+    foreach(QString key, family.keys())
+    {
+        qInfo() << family[key]->getName();
+        qInfo() << family[key]->getAge();
+    }
+    */
+
+    foreach(QString key, family.keys())
+    {
+        QSharedPointer<person> ptr = family.value(key);
+        qInfo() << key << ":" << ptr->getName() << ptr->getAge();
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,8 +51,13 @@ int main(int argc, char *argv[])
     qInfo() << stuInfo;
 
     qInfo() << stuInfo["Yu Han"];
-
+    qInfo() << "-------------------------------------------";
     // Complex Example
+    NickName family;
+    setPersonInstance(family);
+    qInfo() << "Show family info:";
+    showPersonInstance(family);
+
 
 
 
